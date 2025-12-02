0ee3abcecfc749af31b99aac88914aac279b1c20
wnstjd9288 <wnstjd9288@naver.com>
2025-12-02 21:39:01 +0900

    EnemyFSM 기본모델

---
+++ Assets/Scripts/Characters/Enemy/ChaserEnemy.cs
@@ -0,0 +1,35 @@
+public class ChaserEnemy : NormalEnemy
+{
+    protected override void PatrolUpdate()
+    {
+        if (PlayerInRange(detectRange) || isRage)
+        {
+            idlePatrolTimer = 0;
+            state = EnemyState.Chase;
+        }
+        idlePatrolTimer += Time.deltaTime;
+
+        if (idlePatrolTimer >= idlePatrolInterval)
+        {
+            idlePatrolTimer = 0f;
+
+            if (Probability.Attempt(patrolToIdleChance))
+            {
+                state = EnemyState.Idle;
+                return;
+            }
+        }
+        // 일반 Patrol 행동(이동 등)
+    }
+
+    protected override void ChaseUpdate()
+    {
+        isRage = true;
+
+        if (PlayerInRange(attackRange))
+        {
+            state = EnemyState.Attack;
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/Enemy/EnemyState.cs
@@ -0,0 +1,4 @@
+public enum EnemyState
+{
+    Idle, Patrol, Chase, Attack, Frozen
+}
