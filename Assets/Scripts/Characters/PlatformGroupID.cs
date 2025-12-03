928c03df35fff707f14b3f37cd4fd4a91966950a
wnstjd9288 <wnstjd9288@naver.com>
2025-12-03 22:40:11 +0900

    FSM Idle, Patrol, Chase 완성

---
+++ Assets/Scripts/Characters/PlatformGroupID.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PlatformGroupID : MonoBehaviour
+{
+    public int platformGroupID = -1;
+    public bool leftBlocked = false;
+    public bool rightBlocked = false;
+}
