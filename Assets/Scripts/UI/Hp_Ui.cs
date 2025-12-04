6ee80477d20872b7c9552a2dca8596d78ecdaf24
Ian <wowsbmiruan@gmail.com>
2025-12-05 01:07:35 +0900

    HpUIMerge

--- Assets/Scripts/UI/Hp_Ui.cs
+++ Assets/Scripts/UI/Hp_Ui.cs
@@ -5,45 +5,26 @@ using TMPro;
 public class HPBarController : MonoBehaviour
 {
     public Image hpImage;   // Fill Amount 조절할 이미지
-
-    int currentHp;
-    int maxHp = 100;
+    public Player player;
+    private float currentHp;
+    private float maxHp;
     void Start()
     {
+        maxHp = player.status.GetFinal(StatId.HP);
         currentHp = maxHp;
         UpdateHPBar();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            TakeDamage(5);
-        }
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            TakeHeal(5);
-        }
-    }
-
-    public void TakeDamage(int damage)
-    {
-        currentHp -= damage;
-        currentHp = Mathf.Clamp(currentHp, 0, maxHp); // HP 범위 제한
-        UpdateHPBar();
-    }
-
-    public void TakeHeal(int healling)
-    {
-        currentHp += healling;
-        currentHp = Mathf.Clamp(currentHp, 0, maxHp); // HP 범위 제한
-        Debug.Log("회복된 hp : " + healling);
-        UpdateHPBar();
+        maxHp = player.status.GetFinal(StatId.HP);
+        currentHp = player.status.CurrentHP;
+        hpImage.fillAmount = currentHp / maxHp;
     }
 
     void UpdateHPBar()
     {
         // 현재 체력 비율 계산해서 fillAmount에 반영
-        hpImage.fillAmount = (float)currentHp / maxHp;
+        hpImage.fillAmount = currentHp / maxHp;
     }
 }
